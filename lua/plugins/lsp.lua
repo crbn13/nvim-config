30eb2b22e022ab10ce180776070b0b20c9127440
rory <roryredking8@gmail.com>
2025-03-23 12:46:10 +0000

    seems to be working, not added cpp lsp yet

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "cpp",
+        "c",
+      })
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+      },
+    },
+  },
+}
