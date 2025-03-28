1a1203c9e4fb979a278d6584a11bbcb9ab350f7d
rory <roryredking8@gmail.com>
2025-03-28 17:35:42 +0000

    some stuff

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -9,12 +9,14 @@
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "cpp", "hpp", "tpp" },
   callback = function()
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.expandtab = true
+    vim.opt_local.autoindent = true
+    vim.opt_local.smarttab = true
     vim.b.autoformat = false
     vim.b.inlay_hints = false
     vim.cmd("LspStart")
+    vim.cmd("TSDisable indent")
   end,
-})
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "cpp", "hpp", "tpp" },
-  command = "setlocal tabstop=4 shiftwidth=4 expandtab autoindent smarttab filetype=cpp",
 })
