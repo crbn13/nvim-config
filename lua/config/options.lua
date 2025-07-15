c5c5fd2bf85041d4fe8fb0a42622cdb8aafd3467
rory <rory@mail.com>
2025-07-15 20:50:44 +0100

    fixed clangd

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.lsp.enable('clangd')
 vim.api.nvim_set_option_value("clipboard", "unnamed", { scope = "global" })
 vim.api.nvim_set_option_value("wrap", true, { scope = "global" })
 vim.g.autoformat = false
